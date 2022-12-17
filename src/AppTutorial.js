9f01dfbb6915d13ae3413451167251b7386855db
Biswadeep Basu <biswadeep.basu@capgemini.com>
2022-12-18 00:06:05 +0100

    initial commit

---
+++ src/AppTutorial.js
@@ -0,0 +1,42 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route, Link } from "react-router-dom";
+
+import AddTutorial from "./components/Tutorial/AddTutorial";
+import Tutorial from "./components/Tutorial/Tutorial";
+import TutorialsList from "./components/Tutorial/TutorialsList";
+
+function App() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/tutorials" className="navbar-brand">
+          ReactApp
+        </a>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/tutorials"} className="nav-link">
+              Tutorials
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/add"} className="nav-link">
+              Add
+            </Link>
+          </li>
+        </div>
+      </nav>
+
+      <div className="container mt-3">
+        <Routes>
+          <Route path="/" element={<TutorialsList/>} />
+          <Route path="/tutorials" element={<TutorialsList/>} />
+          <Route path="/add" element={<AddTutorial/>} />
+          <Route path="/tutorials/:id" element={<Tutorial/>} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
